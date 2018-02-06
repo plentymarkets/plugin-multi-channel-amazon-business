94273069787a3c690ae2e603015120a3a02578da
Marius Bippig <marius.bippig@plentymarkets.com>
2018-02-06 17:46:23 +0100

    added service provider, adjusted plugin name and renamed info files

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MultiChannelAmazonBusiness;
+
+use Plenty\Plugin\ServiceProvider as PluginServiceProvider;
+
+class ServiceProvider extends PluginServiceProvider
+{
+    public function register()
+    {
+
+    }
+}
